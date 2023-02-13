eaf6389b26896b1da4c99c9669a0cdee39a83294
George <georgehender997@gmail.com>
2023-02-13 17:48:35 -0500

    mid

--- react-rainbow/src/App.js
+++ react-rainbow/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
+import ColorBlock from './ColorBlock';
 
 function App() {
+  let [colors, setColors] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+  let colorMap = colors.map((color, i) => {
+    return (
+      <ColorBlock color={color} />
+    )
+  })
   return (
     <div className="App">
-      
+      {colors.map((color, i) =>
+        <ColorBlock key="" color={color} />
+      )}
     </div>
   );
 }
